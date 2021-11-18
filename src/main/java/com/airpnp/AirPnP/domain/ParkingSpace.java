55bd2b9adb5a02e19d9452089e06f85a0ac64595
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-18 15:09:51 +0100

    Add ParkingSpace class.

---
+++ src/main/java/com/airpnp/AirPnP/domain/ParkingSpace.java
@@ -0,0 +1,40 @@
+package com.airpnp.AirPnP.domain;
+
+import java.util.Date;
+
+public class ParkingSpace {
+
+    private int id;
+    private int price;
+    private Date startDate;
+    private Date endDate;
+    private String streetAddress;
+
+    public ParkingSpace(int id, int price, Date startDate, Date endDate, String streetAddress) {
+        this.id = id;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.streetAddress = streetAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+}
