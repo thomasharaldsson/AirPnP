3b68bef0be5f46104f86b3c657920978f67ba64a
uzair-c <hej@uzair.se>
2021-11-18 10:52:43 +0100

    created spring project

---
+++ src/main/java/com/airpnp/AirPnP/AirPnPApplication.java
@@ -0,0 +1,13 @@
+package com.airpnp.AirPnP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPnPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPnPApplication.class, args);
+	}
+
+}
