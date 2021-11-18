27c5ee59435d5ff56a3753459873f32432c91cba
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-18 16:38:44 +0100

    Add Repository notation to CustomerRepository.java.

--- src/main/java/com/airpnp/AirPnP/data/CustomerRepository.java
+++ src/main/java/com/airpnp/AirPnP/data/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.airpnp.AirPnP.data;
 import com.airpnp.AirPnP.domain.Customer;
 import com.airpnp.AirPnP.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
