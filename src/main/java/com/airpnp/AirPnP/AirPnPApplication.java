ca9610fc53063a5a95bd0f6217f1ef6b30278b68
thomasharaldsson <thomas.haraldsson5@gmail.com>
2021-11-19 13:39:15 +0100

    Moved main method outside package

--- src/main/java/com/airpnp/AirPnP/AirPnPApplication.java
+++ src/main/java/com/airpnp/AirPnP/AirPnPApplication.java
@@ -1,4 +1,4 @@
-package com.airpnp.AirPnP.client;
+package com.airpnp.AirPnP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
