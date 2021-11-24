fb043e6bf5a53a613c6bfca0960c0d88f521e24b
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-24 13:52:50 +0100

    Add repository to ParkingSpaceProductionImplementation.

--- src/main/java/com/airpnp/service/ParkingSpaceProductionImplementation.java
+++ src/main/java/com/airpnp/service/ParkingSpaceProductionImplementation.java
@@ -1,10 +1,16 @@
 package com.airpnp.service;
 
+import com.airpnp.data.ParkingSpaceRepository;
 import com.airpnp.domainmodel.ParkingSpace;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class ParkingSpaceProductionImplementation implements ParkingSpaceService {
+
+    @Autowired
+    private ParkingSpaceRepository data;
+
     @Override
     public ParkingSpace getParkingSpaceById(int Id) {
         // TODO: Implement method
