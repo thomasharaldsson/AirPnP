3b753ad5a9d77028ebe669f9999c398c276cf756
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-24 13:48:55 +0100

    Add ParkingSpaceRepository.

---
+++ src/main/java/com/airpnp/data/ParkingSpaceRepository.java
@@ -0,0 +1,9 @@
+package com.airpnp.data;
+
+import com.airpnp.domainmodel.ParkingSpace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
+}
