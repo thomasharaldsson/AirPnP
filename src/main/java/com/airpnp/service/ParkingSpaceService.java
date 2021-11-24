4cc10376d1950608ef65244af7de70f24dd64e8c
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-24 12:18:50 +0100

    Remove Repository annotation from ParkingSpaceController.

--- src/main/java/com/airpnp/service/ParkingSpaceService.java
+++ src/main/java/com/airpnp/service/ParkingSpaceService.java
@@ -1,11 +1,9 @@
 package com.airpnp.service;
 
 import com.airpnp.domainmodel.ParkingSpace;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ParkingSpaceService {
 
     public ParkingSpace getParkingSpaceById(int Id);
