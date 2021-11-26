24e0755493ebb1cdc8db4fdf1662b2fec9b3fc5b
mehrdadbadeie <Newmedia10>
2021-11-26 10:31:07 +0100

    Created a simple login for the application. It is now set to developermode (bypassing login)
    To enable login function, in application.properties set spring.profiles.active to production.
    
    Username: user
    Password: password

---
+++ src/main/java/com/airpnp/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.airpnp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Profile("production")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
