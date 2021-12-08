7d2247f2e2e6cb615141063b375e9752319a0f3f
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-12-08 16:05:11 +0100

    Enable Spring Security @EnableGlobalMethodSecurity.

--- src/main/java/com/airpnp/security/SecurityConfig.java
+++ src/main/java/com/airpnp/security/SecurityConfig.java
@@ -1,15 +1,36 @@
 package com.airpnp.security;
 
+import com.airpnp.domainmodel.Customer;
+import com.airpnp.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 @Profile("production")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String ROLE_CUSTOMER = "CUSTOMER";
+    private static final String ROLE_LENDER = "LENDER";
+    public static final String USER_ROLE_CUSTOMER = "ROLE_" + ROLE_CUSTOMER;
+    public static final String USER_ROLE_LENDER = "ROLE_" + ROLE_LENDER;
+
+    @Autowired
+    private CustomerService customerService;
 
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -19,6 +40,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("userCustomer")
+                .password(encoder.encode("salasana1"))
+                .roles(ROLE_CUSTOMER)
+                .and()
+                .withUser("userLender")
+                .password(encoder.encode("salasana2"))
+                .roles(ROLE_LENDER);
+
+        List<Customer> allCustomers = customerService.getAll();
+        for (Customer c : allCustomers) {
+            System.out.println("Grant login access to user " + c);
+        }
     }
 
 }
