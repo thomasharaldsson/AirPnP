df0bd97116b20215a1df0a7c62a29503bb6b60e7
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-12-18 10:49:34 +0100

    Change class name to something more appropriate.

--- src/main/java/com/airpnp/controller/indexPageController.java
+++ src/main/java/com/airpnp/controller/indexPageController.java
@@ -1,15 +1,13 @@
 package com.airpnp.controller;
 
-import com.airpnp.data.exception.ParkingSpaceNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
-public class startPageController {
+public class indexPageController {
     @GetMapping
     public ModelAndView showPage() {
         System.out.println("Startpage");
