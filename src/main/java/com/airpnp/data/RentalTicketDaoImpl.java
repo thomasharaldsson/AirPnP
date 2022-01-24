bd1d53ddb5604cd016ca9c38eb344a9880b45471
Thomas Haraldsson <thohar2001@skola.goteborg.se>
2022-01-24 15:04:40 +0100

    Added entitymanager and rentalticketrepository for rentalticketdaoimpl

--- src/main/java/com/airpnp/data/RentalTicketDaoImpl.java
+++ src/main/java/com/airpnp/data/RentalTicketDaoImpl.java
@@ -1,14 +1,28 @@
 package com.airpnp.data;
 
 import com.airpnp.data.exception.RentalTicketNotFoundException;
+import com.airpnp.data.repository.RentalTicketRepository;
+import com.airpnp.data.repository.VehicleRepository;
 import com.airpnp.domainmodel.RentalTicket;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 public class RentalTicketDaoImpl implements RentalTicketDao {
+
+    @Autowired
+    RentalTicketRepository data;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final static String JPA_QUERY_GET_RENTAL_TICKETS_FOR_CUSTOMER = "SELECT v FROM Vehicle v WHERE v.owner =: customer";
+
     @Override
     public void addRentalTicket(RentalTicket newRentalTicket) {
-        
+
     }
 
     @Override
