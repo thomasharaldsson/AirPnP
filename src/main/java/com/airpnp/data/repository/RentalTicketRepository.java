6891749d587ce243883fd3e186a26cee78ef64f1
Thomas Haraldsson <thohar2001@skola.goteborg.se>
2022-01-24 15:05:07 +0100

    Updated rentalticketrepository

--- src/main/java/com/airpnp/data/repository/RentalTicketRepository.java
+++ src/main/java/com/airpnp/data/repository/RentalTicketRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface RentalTicketRepository extends JpaRepository<RentalTicket, Integer> {
-    void deleteRentalTicketById(Integer id);
 }
